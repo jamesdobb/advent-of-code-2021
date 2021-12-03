9bcf67152937de8cdf7506a9d9a61c16f6bc2b6a
James Dobb <james.dobb@zoopla.co.uk>
2021-12-03 08:38:40 +0000

    whoops - commit all

--- day3/part1/main.go
+++ day3/part1/main.go
@@ -3,30 +3,43 @@ package main
 import (
 	"advent-of-code/shared"
 	"fmt"
-	"log"
-	"strings"
+	"strconv"
 )
 
 func main() {
 	scanner := shared.NewScanner("./input.txt")
-	depth, forward, aim := 0, 0, 0
+	var t, f = make(map[int]int), make(map[int]int)
 
 	for scanner.Scan() {
-		parts := strings.Split(scanner.Text(), " ")
-		v := shared.StringToInt(parts[1])
-
-		switch parts[0] {
-		case "forward":
-			forward += v
-			depth += aim * v
-		case "down":
-			aim += v
-		case "up":
-			aim -= v
-		default:
-			log.Fatalf("failed to handle command: %v", parts[0])
+		v := scanner.Text()
+		for i, c := range v {
+			if fmt.Sprintf("%c", c) == "1" {
+				t[i]++
+			} else {
+				f[i]++
+			}
 		}
 	}
 
-	fmt.Println(depth * forward)
+	gr, er := "", ""
+	for i := 0; i < len(t); i++ {
+		if t[i] == f[i] {
+			panic("I don't know what to do here!")
+		} else if t[i] > f[i] {
+			gr += "1"
+			er += "0"
+		} else {
+			gr += "0"
+			er += "1"
+		}
+	}
+
+	grV, _ := strconv.ParseInt(gr, 2, 64)
+	erV, _ := strconv.ParseInt(er, 2, 64)
+
+
+	fmt.Println(grV)
+	fmt.Println(erV)
+
+	fmt.Println(grV*erV)
 }
